14a329cb0315eb4b94ccb77811d70fb074a113a0
Connor <chan2092@umn.edu>
2024-03-18 13:37:28 +0100

    Added TypeScript folder and first file

---
+++ ts/agent.ts
@@ -0,0 +1,19 @@
+/*
+ * chan2092
+ * ts/agent.ts
+ *
+ * Logic for playing 2048 intelligently
+ */
+
+interface State {
+
+}
+
+interface Grid {
+    size: number;
+    cells: State;
+}
+
+class Agent {
+    grid: Grid;
+}
